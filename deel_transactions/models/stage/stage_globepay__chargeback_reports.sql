fd8c2695b99b9af6c0f2371761972b79abdf290b
Ziae Mustafa <ziae.mustafa@rnanalytics.tech>
2024-08-14 15:43:48 +0400

    feat: Adding primary key and tests to stage models

--- deel_transactions/models/stage/stage_globepay__chargeback_reports.sql
+++ deel_transactions/models/stage/stage_globepay__chargeback_reports.sql
@@ -1,6 +1,16 @@
-select
-    external_ref
-    , status
-    , source
-    , chargeback
-from {{ source('transactions', 'globepay_chargeback_report') }}
+WITH
+  globepay_chargeback AS (
+  SELECT
+    external_ref,
+    status,
+    SOURCE,
+    chargeback
+  FROM
+    {{ source('transactions', 'globepay_chargeback_report') }}
+)
+
+SELECT
+  {{ dbt_utils.generate_surrogate_key(['external_ref']) }} as PK_STAGE_GLOBEPAY__CHARGEBACK_REPORTS,
+  *
+FROM
+  globepay_chargeback
